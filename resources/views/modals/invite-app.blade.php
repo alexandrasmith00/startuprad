b4e673a95757c5d173c5ad3cb9a08a30e9afdbe2
Lexi <alexandrasmith00@gmail.com>
2016-09-15 17:40:07 -0400

    style for invite box

--- resources/views/modals/invite-app.blade.php
+++ resources/views/modals/invite-app.blade.php
@@ -1,8 +1,7 @@
 <div class="modal fade" id="invite-modal" tabindex="-1" role="dialog" aria-labelledby="invite-modal">
-  <div style="margin-top: 10%; background-color: white;" class="holder col-xs-12 col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+  <div style="margin-top: 100px; margin-bottom: 100px; position: absolute; overflow: scroll;background-color: white;" class="holder col-xs-12 col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
       <button style="margin-top: 10px;" type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
       <br>
-
 
         <h3>Invite these teams</h3>
 
@@ -17,7 +16,7 @@
         @endif
 
         <form action="{{ route('sending-student-invites') }}">
-          <button style="margin-top: 50px;" class="btn oversized-btn btn-blue" type="submit">Invite</button>
+          <button style="margin-top: 50px; margin-bottom: 25px;" class="btn oversized-btn btn-blue" type="submit">Invite</button>
         </form>
 
   </div>
