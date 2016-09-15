699eabd632b28a4dcc218cb21a70a7cd3e14aa28
Lexi <alexandrasmith00@gmail.com>
2016-09-14 21:43:35 -0400

    commentable issue

--- config/app.php
+++ config/app.php
@@ -153,7 +153,6 @@ return [
         Aws\Laravel\AwsServiceProvider::class,
         Barryvdh\Snappy\ServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
-        Slynova\Commentable\ServiceProvider::class,
 
 
 
